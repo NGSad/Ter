3bf20da429c1b7b14691d7f7fb0878a837faf59d
南宫伤心 <1124057868@qq.com>
2018-08-15 16:19:09 +0800

    0.0

---
+++ vueapp1/src/views/movie/MovieDetail.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1>{{detail.tatle}}</h1>
+    <p>{{detail.summary}}</p>
+</template>
+
+<script>
+    import axios from "axios";
+    export default {
+        data(){
+            return{
+                detail:{}
+            }
+        },
+        created(){
+            let url=`https://bird.ioliu.cn/v2?url=https://api.douban.com/v2/movie/subject/${
+                this.$route.params.movieId
+                }`;
+            axios.get(url).then(res =>{
+                this.detail = res.data;
+            });
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
